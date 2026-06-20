14d95db10821adf06f156089ae70ccb938522958
khadoujkenn <khadoujkenn@example.com>
2026-06-20 16:29:54 +0100

    update styling and layout

--- script.js
+++ script.js
@@ -10,16 +10,19 @@ searchButton.addEventListener('click', () => {
     const query = searchInput.value;
     if (!query) return;
 
-const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=30&client_id=${apiKey}`;
+const url =` https://api.unsplash.com/search/photos?query=${query}&per_page=30&order_by=relevant&client_id=${apiKey};`
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            resultsContainer.innerHTML = '';
-            data.results.forEach(photo => {
-                const img = document.createElement('img');
-                img.src = photo.urls.regular;
-                resultsContainer.appendChild(img);
+           data.results.forEach(photo => {
+    const div = document.createElement('div'); // إنشاء حاوية لكل صورة
+    div.classList.add('image-card'); // إضافة كلاس للتحكم به في CSS
+    const img = document.createElement('img');
+    img.src = photo.urls.regular;
+    div.appendChild(img);
+    resultsContainer.appendChild(div);
+
             });
         })
         .catch(error => console.error('خطأ في الاتصال:', error));
